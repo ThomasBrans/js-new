1a93fdbe44e0d4ddb4361f6ae9efb3dc0d9433a3
theonlytbboy <thomasbrans.tb@gmail.com>
2019-07-30 11:30:14 +0200

    new commit$

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+//exercise 2//
+const Exercise1 = document.getElementByID('Exercise1');
+
 function me(name, lastname, age, favoritefood, favoriteseries, favoritefilm){
 this.name = name;
 this.lastname = lastname;
@@ -13,4 +17,22 @@ return ++this.age;
 
 let me = new Me(thomas, brans, 21, dono, altered_carbon, matrix );
 
+//*exersice 2//
+const Exercise2 = document.getElementById('Exercise2');
+
 console.log(me.updateAge())
+function femaly(elmari, jo, kathlyn){
+this.dad = dad;
+this.mom = mom;
+this.sister = sister;
+};z
+
+let dad = new member ( "jo" ,47 );
+let mom = new member ("elmari" , 46);
+let sister = new member ("kathlyn" ,19)
+
+let famely = document.getElementById('famely');
+
+const Exercise3 = Document.getElementById('exercise3');
+
+//why this doesnt work
